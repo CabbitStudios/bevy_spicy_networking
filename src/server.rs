71ec7f14a83c1a6a92a49c8cbfc38db3490d187f
rezural <rezural@protonmail.com>
2021-12-06 15:46:24 +1100

    changes to library for bevy 0.6

--- src/server.rs
+++ src/server.rs
@@ -378,9 +378,9 @@ pub trait AppNetworkServerMessage {
     fn listen_for_server_message<T: ServerMessage>(&mut self) -> &mut Self;
 }
 
-impl AppNetworkServerMessage for AppBuilder {
+impl AppNetworkServerMessage for App {
     fn listen_for_server_message<T: ServerMessage>(&mut self) -> &mut Self {
-        let server = self.world().get_resource::<NetworkServer>().expect("Could not find `NetworkServer`. Be sure to include the `ServerPlugin` before listening for server messages.");
+        let server = self.world.get_resource::<NetworkServer>().expect("Could not find `NetworkServer`. Be sure to include the `ServerPlugin` before listening for server messages.");
 
         debug!("Registered a new ServerMessage: {}", T::NAME);
 
